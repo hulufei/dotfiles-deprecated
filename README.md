20f0ce402cdc4ce2c86f6948db3a2e96dd2044b3
hulufei <f.calabash@gmail.com>
2014-07-20 21:55:43 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+About
+======
+
+Personal configuration and necessary files for Linux/Mac
+
+- .extra is a custom file for [mathiasbynens/dotfiles](https://github.com/mathiasbynens/dotfiles)
+- Move .dropbox-dist/ to ~/ to install dropbox in Linux
+
+**Tips**
+
+- Ack search specified file: eg. --xml
+
+> oh-my-zsh's internals are defined in its lib directory. To change them just
+> create a file inside the custom directory (its name doesn't matter, as long
+> as it has a .zsh ending) and start customizing whatever you want.
+
+**Mac**
+
+- [quick-look-plugins](https://github.com/sindresorhus/quick-look-plugins)
---
+++ custom.zsh
@@ -0,0 +1,17 @@
+# git alias
+git config --global alias.undo-commit 'reset --soft HEAD^'
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --global alias.amend-commit 'git commit --amend -C HEAD'
+
+# Tmuxifier
+export EDITOR=vim
+export PATH="$HOME/.tmuxifier/bin:$PATH"
+eval "$(tmuxifier init -)"
+
+# fasd
+eval "$(fasd --init auto)"
+alias v='f -e vim' # quick opening files with vim
+alias m='f -e mplayer' # quick opening files with mplayer
+
+# so as not to be disturbed by Ctrl-S ctrl-Q in terminals
+stty -ixon
