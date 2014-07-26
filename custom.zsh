559ad60b4d6f506f5ff2406f5b0cf9067f8c81e1
hui.liu <f.calabash@gmail.com>
2014-07-26 14:27:31 +0800

    Fix tmux status-utf8 issue

--- custom.zsh
+++ custom.zsh
@@ -5,6 +5,10 @@ git config --global alias.undo-commit 'reset --soft HEAD^'
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 git config --global alias.amend-commit 'git commit --amend -C HEAD'
 
+# Fix tmux status-utf8 issue, or force tmux to use utf8: `tmux -u`
+# http://goo.gl/hxBM6q
+export LANG=en_us.UTF-8
+
 # Tmuxifier
 export EDITOR=vim
 export PATH="$HOME/.tmuxifier/bin:$PATH"
